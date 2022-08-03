f9d60afbaec6564813197b108ce0ca5eca33aa84
abhishek1901ee04 <54908876+abhishek1901ee04@users.noreply.github.com>
2022-08-03 09:54:27 +0530

    recentdev

---
+++ lecture8.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+// agar hum kisi overloaded constructor main sarre parameter ki defualt value pass karde to wo default constructor ban jaata hai phir hum defualt ko hata sakte hai
+class humanBeing{
+    private:
+    int age;
+    string name;
+public:
+    // humanBeing(){
+    //     cout<<"default constructor "<<endl;
+    //     age =0;
+    //     name = "noname";
+    // }
+   
+    humanBeing(string iname="anil" , int iage=0){
+        cout<<"constructor with name and age as the parameter" <<endl;
+        name = iname;
+        age = iage;
+    }
+    void display(){
+        cout<<name<<endl<<age<<endl;
+    }
+};
+int main(){
+    humanBeing man;
+    man.display();
+    humanBeing man1("ankit");
+    man1.display();
+    humanBeing man2("amit",14);
+       man2.display();
+ 
+ 
+    return 0;
+}
